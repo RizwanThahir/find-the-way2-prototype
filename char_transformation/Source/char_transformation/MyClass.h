64ce1916a4353503d10ff11a7e94a6e5fac12509
RizwanThahir <grimreper054@gmail.com>
2025-11-06 19:39:06 +0530

    prototype

---
+++ char_transformation/Source/char_transformation/MyClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class CHAR_TRANSFORMATION_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
